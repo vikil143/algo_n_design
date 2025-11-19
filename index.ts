77624409c7d91dac503ce3085c2d3ef530a267d0
vikil143 <vikil.lakkavatri25@gmail.com>
2025-11-19 11:13:02 +0530

    first problem was solved

--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+import { findLargestElement } from "./src/algorithm/Q1"
+
+function main() {
+    const numbers = [3, 5, 7, 2, 8];
+    const largestNumber = findLargestElement(numbers);
+    console.log("Question 1 Solution:");
+    console.log(`The largest number is: ${largestNumber}`); // Output: The largest number is: 8
+}
+
+main();
