3033e47d94278fd13dec4e729b09200fbfd72861
vikil143 <vikil.lakkavatri25@gmail.com>
2026-01-12 18:35:02 +0530

    missing array solution was done

--- src/topmate_prep/basicOperations.ts
+++ src/topmate_prep/basicOperations.ts
@@ -41,3 +41,31 @@ export const checkArraySorted = (arr: number[]): boolean => {
     }
     return true;
 }
+
+// Find the missing number in an array 
+export const missingNumberInArray = (arr: number[]): number => {
+    const n = arr.length;
+    const expectedSum = (n * (n + 1)) / 2;
+    const actualSum = arr.reduce((acc, num) => acc + num, 0);
+    console.log({expectedSum, actualSum});
+    return expectedSum - actualSum;
+}
+
+// Find the missing number in an array [0, 1, 2, 4, 5] => 3
+// Solution Two
+
+export const solutionMissingNumberInArray = (arr: number[]): number => {
+    const n = arr.length;
+    let xorFull = 0;
+    let xorArr = 0;
+
+    for (let i = 0; i <= n; i++) {
+        xorFull ^= i;
+    }
+
+    for (let num of arr) {
+        xorArr ^= num;
+    }
+
+    return xorFull ^ xorArr;
+}
--- src/topmate_prep/index.ts
+++ src/topmate_prep/index.ts
@@ -1,4 +1,4 @@
-import { checkArraySorted, largestElement, secondLargestElement } from "./basicOperations";
+import { checkArraySorted, largestElement, missingNumberInArray, secondLargestElement, solutionMissingNumberInArray } from "./basicOperations";
 
 export function topMateMain() {
     const array = [3, 5, 7, 8, 2];
@@ -18,4 +18,18 @@ export function topMateMain() {
     const isSorted2 = checkArraySorted(arraySorted);
     console.log(`Is the array sorted? : ${isSorted2}`);
     console.log("-----------------------");
+    const arrayWithMissingNumber = [0, 1, 2, 4, 5];
+    console.log("Finding the missing number in the array:", arrayWithMissingNumber);
+    const missingNumber = missingNumberInArray(arrayWithMissingNumber);
+    console.log(`The missing number in the array is: ${missingNumber}`);
+    console.log("-----------------------");
+    const arrayWithMissingNumber2 = [1, 2, 3, 5, 0];
+    console.log("Finding the missing number in the array:", arrayWithMissingNumber2);
+    const missingNumber2 = missingNumberInArray(arrayWithMissingNumber2);
+    console.log(`The missing number in the array is: ${missingNumber2}`);
+    console.log("-----------------------");
+    const arrayWithMissingNumber3 = [0, 1, 3];
+    console.log("Finding the missing number in the array:", arrayWithMissingNumber3);
+    const missingNumber3 = solutionMissingNumberInArray(arrayWithMissingNumber3);
+    console.log(`The missing number in the array is: ${missingNumber3}`);
 }
